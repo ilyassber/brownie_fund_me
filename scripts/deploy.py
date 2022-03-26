2f28184330f190386baca19a83d285c3d1a7fa48
ilyassber <ilyass.berchida@gmail.com>
2022-03-26 23:52:20 +0100

    add networks to the config file

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,11 +1,16 @@
-from brownie import FundMe
+from brownie import FundMe, network, config
 from scripts.helpful_scripts import get_account
 
 
 def deploy_find_me():
     account = get_account()
+    if network.show_active() != "developement":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+
     fund_me = FundMe.deploy(
-        "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
+        price_feed_address,
         {"from": account},
         publish_source=True,
     )
